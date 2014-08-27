1cd1b4d4fcc2ef2e1a501ea2815036a83b6a3c78
Gaurav Mukherjee <gdm284@nyu.edu>
2014-08-27 14:21:55 -0400

    Android APP

---
+++ Wellnesswaves/app/src/main/java/blueocean/wellnesswaves/app/CView.java
@@ -0,0 +1,33 @@
+package blueocean.wellnesswaves.app;
+
+/**
+ * Created by Gaurav on 8/26/14.
+ */
+
+import android.content.Context;
+import android.widget.ImageView;
+
+
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import blueocean.wellnesswaves.app.R;
+
+/**
+ * Nothing but an ImageView with a preset image resource
+ *
+ * @author yildizkabaran
+ */
+public class CView extends ImageView {
+
+    public CView(Context context) {
+        super(context);
+        initialize();
+    }
+
+    private void initialize() {
+        // set the dummy content image here
+        // setImageResource();
+    }
+}
---
+++ Wellnesswaves/app/src/main/java/blueocean/wellnesswaves/app/MainActivity.java
@@ -0,0 +1,121 @@
+package blueocean.wellnesswaves.app;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import java.util.Random;
+
+
+
+import java.util.Random;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+
+import blueocean.wellnesswaves.app.CView;
+import blueocean.wellnesswaves.app.SView;
+import blueocean.wellnesswaves.app.SView.ISplashListener;
+
+public class MainActivity extends Activity {
+
+    private static final String TAG = "MainActivity";
+    private static final boolean DO_XML = false;
+
+    private ViewGroup mMainView;
+    private SView mSplashView;
+    private View mContentView;
+    private Handler mHandler = new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // change the DO_XML variable to switch between code and xml
+        if (DO_XML) {
+            // inflate the view from XML and then get a reference to it
+            setContentView(R.layout.activity_main);
+            mMainView = (ViewGroup) findViewById(R.id.main_view);
+            mSplashView = (SView) findViewById(R.id.splash_view);
+        } else {
+            // create the main view
+            mMainView = new FrameLayout(getApplicationContext());
+
+            // create the splash view
+            mSplashView = new SView(getApplicationContext());
+            mSplashView.setRemoveFromParentOnEnd(true); // remove the SplashView from MainView once animation is completed
+            mSplashView.setSplashBackgroundColor(getResources().getColor(R.color.splash_bg)); // the background color of the view
+            mSplashView.setRotationRadius(getResources().getDimensionPixelOffset(R.dimen.splash_rotation_radius)); // radius of the big circle that the little circles will rotate on
+            mSplashView.setCircleRadius(getResources().getDimensionPixelSize(R.dimen.splash_circle_radius)); // radius of each circle
+            mSplashView.setRotationDuration(getResources().getInteger(R.integer.splash_rotation_duration)); // time for one rotation to be completed by the small circles
+            mSplashView.setSplashDuration(getResources().getInteger(R.integer.splash_duration)); // total time taken for the circles to merge together and disappear
+            mSplashView.setCircleColors(getResources().getIntArray(R.array.splash_circle_colors)); // the colors of each circle in order
+
+            // add splash view to the parent view
+            mMainView.addView(mSplashView);
+            setContentView(mMainView);
+        }
+
+        // pretend like we are loading data
+        startLoadingData();
+    }
+
+    private void startLoadingData() {
+        // finish "loading data" in a random time between 1 and 3 seconds
+        Random random = new Random();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                onLoadingDataEnded();
+            }
+        }, 1000 + random.nextInt(2000));
+    }
+
+    private void onLoadingDataEnded() {
+        Context context = getApplicationContext();
+        // now that our data is loaded we can initialize the content view
+        mContentView = new CView(context);
+        // add the content view to the background
+        mMainView.addView(mContentView, 0);
+
+        // start the splash animation
+        mSplashView.splashAndDisappear(new ISplashListener() {
+            @Override
+            public void onStart() {
+                // log the animation start event
+                if (BuildConfig.DEBUG) {
+                    Log.d(TAG, "splash started");
+                }
+            }
+
+            @Override
+            public void onUpdate(float completionFraction) {
+                // log animation update events
+                if (BuildConfig.DEBUG) {
+                    Log.d(TAG, "splash at " + String.format("%.2f", (completionFraction * 100)) + "%");
+                }
+            }
+
+            @Override
+            public void onEnd() {
+                // log the animation end event
+                if (BuildConfig.DEBUG) {
+                    Log.d(TAG, "splash ended");
+                }
+                // free the view so that it turns into garbage
+                mSplashView = null;
+            }
+        });
+    }
+}
